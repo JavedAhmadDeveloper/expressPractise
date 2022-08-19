39a45ddf120b4da6b09128a74a1fbdea27b720df
JavedAhmadDeveloper <javed.ahmad@mergestack.com>
2022-08-19 16:01:42 +0500

    route added with multiple request method

--- index.js
+++ index.js
@@ -16,6 +16,30 @@ app.get("/ab*cd", (req, res) => {
 app.get("/user", (req, res) => {
   res.send("Got a get request at /user");
 });
+// more than 1 handlers
+// end with fly
+app.get(
+  /.*fly$/,
+  (req, res, next) => {
+    console.log("the response will be sent by the next function ...");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello from 2nd handler of fly!");
+  }
+);
+
+app
+  .route("/book")
+  .get((req, res) => {
+    res.send("Get a random book");
+  })
+  .post((req, res) => {
+    res.send("Add a book");
+  })
+  .put((req, res) => {
+    res.send("Update the book");
+  });
 
 app.listen(PORT, () =>
   console.log(`app is Running on http://localhost:${PORT}`)
